7b19ca10f7e3182a1cd8a1b8c51a29b618b43f72
JesseLivezey <jesse.livezey@gmail.com>
2013-07-31 10:55:14 -0700

    Updated sim

--- sim/fallingslinky.py
+++ sim/fallingslinky.py
@@ -1,12 +1,19 @@
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 def main():
-    nMasses = 1000
-    k = 1000.
-    m = .0002
+    saveFile = 'slinkySim.csv'
+    length = 1.3
+    totMass = .21
+    nMasses = 4
+    collisions = True 
     g = 9.8
     dt = .0002
     nIter = 2000
+    plotAll = False
+    #Calculated parameters
+    m = totMass/nMasses
+    k = (nMasses-1)*nMasses*m*g/(2*length)
     masses = np.array([m for ii in xrange(nMasses)])
     collapsed = [False for ii in xrange(nMasses)]
     y = np.zeros((nIter+1,nMasses))
@@ -35,7 +42,7 @@ def main():
                 v[ii+1,jj] = v[ii+1,jj+1]
                 y[ii+1,jj] = y[ii+1,jj+1]
             #Check to see if it has hit lower mass, if so, do inelastic collision
-            if not collapsed[jj] and jj != nMasses-1 and y[ii+1,jj] < y[ii+1,jj+1]:
+            if collisions == True and not collapsed[jj] and jj != nMasses-1 and y[ii+1,jj] < y[ii+1,jj+1]:
                 y[ii+1,jj] = y[ii+1,jj+1]
                 v[ii+1,jj] = (masses[jj]*v[ii+1,jj]+masses[jj+1]*v[ii+1,jj+1])/(masses[jj]+masses[jj+1])
                 v[ii+1,jj+1] = v[ii+1,jj]
@@ -43,12 +50,20 @@ def main():
                 masses[jj] = 0
                 collapsed[jj] = True
 
-    yplots = (t,y[:,0],t,y[:,nMasses-1])
-    vplots = (t,v[:,0],t,v[:,nMasses-1])
-    #for ii in xrange(nMasses):
-    #    yplots += (t,y[:,ii])
-    #    vplots += (t,v[:,ii])
-        
+
+    if plotAll:
+        yplots = ()
+        vplots = ()
+        for ii in xrange(nMasses):
+            yplots += (t,y[:,ii])
+            vplots += (t,v[:,ii])
+    else:
+        yplots = (t,y[:,0],t,y[:,nMasses-1])
+        vplots = (t,v[:,0],t,v[:,nMasses-1])
+    print 'Saving file: '+saveFile
+    output = np.append(y,v,axis=1)
+    with open(saveFile,'w+b') as f:
+       np.savetxt(f,np.concatenate((np.array([t]).T,y,v),axis=1),delimiter=',') 
 
     axy = plt.subplot2grid((2,2),(0,0),colspan=2)
     axy.plot(*yplots)
